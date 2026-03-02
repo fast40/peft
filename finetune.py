48157951f9dd95a1d264e557ba2b2104ff7a7bb9
fast40 <eclfast@gmail.com>
2026-03-02 08:35:29 -0800

    initial training loop

--- finetune.py
+++ finetune.py
@@ -1,16 +1,14 @@
 import pathlib
-import time
-import statistics
 
 from transformers import AutoModelForCausalLM, AutoTokenizer, DataCollatorForLanguageModeling
 from datasets import load_dataset, load_from_disk
 from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
+from torch import optim
 
 SEED = 1337
 TEST_HOLDOUT_RATIO = 0.1
 DATASET_DIR = pathlib.Path('dataset')
-BATCH_SIZE = 1
+BATCH_SIZE = 16
 
 tokenizer = AutoTokenizer.from_pretrained('gpt2')
 tokenizer.pad_token = tokenizer.eos_token
@@ -24,40 +22,26 @@ else:
     dataset.save_to_disk(DATASET_DIR)
 
 
-model = AutoModelForCausalLM.from_pretrained('gpt2')
-
-# training loop
-
 collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
-
-num_workers = 8
 
 dataloader = DataLoader(
     dataset['train'],
-    batch_size=100,
+    batch_size=BATCH_SIZE,
     collate_fn=collator,
     shuffle=True,
     pin_memory=True,
-    num_workers=num_workers,
+    num_workers=4,
 )
 
+model = AutoModelForCausalLM.from_pretrained('gpt2')
 
-average = []
+optimizer = optim.AdamW(model.parameters(), lr=3e-4)
 
-t = time.perf_counter()
-for i, batch in enumerate(dataloader):
-    average.append(time.perf_counter() - t)
-    t = time.perf_counter()
+for batch in dataloader:
+    outputs = model(**batch)
+    
+    optimizer.zero_grad()
+    outputs.loss.backward()
+    optimizer.step()
 
-    if i == 100:
-        break;
-
-print(f'Average for {num_workers=}: {statistics.mean(average[1:])}')
-plt.title(f'Average: {statistics.mean(average[1:])}')
-plt.scatter(range(len(average[1:])), average[1:], marker='.')
-
-for x in range(len(average[1:])):
-    plt.axvline(x, color='gray', alpha=0.3, linewidth=0.5)
-
-plt.savefig('output.png', dpi=200)
-
+    print(outputs.loss)
